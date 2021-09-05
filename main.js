3912e4c2bc1740c35714a527c72999701485b264
KukrejaDhriti <83954166+KukrejaDhriti@users.noreply.github.com>
2021-09-05 14:07:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function setup()
+{
+    video= createCapture(VIDEO);
+    video.size(550, 500);
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+}
+function gotPoses(results)
+{
+if(results.length > 0)
+    {
+        console.log(results);
+    }
+}
